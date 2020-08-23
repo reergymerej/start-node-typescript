33f2b420acf1ac4c1a418368a32fe62fe9846a78
Jeremy Greer <jex.grizzle@gmail.com>
2020-08-23 08:42:53 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+}
